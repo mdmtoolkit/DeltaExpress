1f3326a962ad0cd097862bbf9b160b1766a206ea
mdmtoolkit <80696593+mdmtoolkit@users.noreply.github.com>
2021-04-04 03:32:36 +0500

    Add project files.

---
+++ DeltaExpress/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace DeltaExpress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbSource = new System.Windows.Forms.TabPage();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.cbSourceList = new System.Windows.Forms.ComboBox();
+            this.tbMapping = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtDestinationTableName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tbSource.SuspendLayout();
+            this.tbMapping.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tbSource);
+            this.tabControl1.Controls.Add(this.tbMapping);
+            this.tabControl1.Location = new System.Drawing.Point(21, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(908, 669);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tbSource
+            // 
+            this.tbSource.BackColor = System.Drawing.Color.Silver;
+            this.tbSource.Controls.Add(this.btnNext);
+            this.tbSource.Controls.Add(this.txtFilePath);
+            this.tbSource.Controls.Add(this.btnSelectFile);
+            this.tbSource.Controls.Add(this.cbSourceList);
+            this.tbSource.Location = new System.Drawing.Point(4, 22);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Padding = new System.Windows.Forms.Padding(3);
+            this.tbSource.Size = new System.Drawing.Size(900, 392);
+            this.tbSource.TabIndex = 0;
+            this.tbSource.Text = "Source";
+            this.tbSource.Click += new System.EventHandler(this.tbSource_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(86, 182);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(155, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(166, 145);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbSourceList
+            // 
+            this.cbSourceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSourceList.FormattingEnabled = true;
+            this.cbSourceList.Items.AddRange(new object[] {
+            "Select Source Type",
+            "CSV",
+            "Excel",
+            "Azure Synape",
+            "SAP"});
+            this.cbSourceList.Location = new System.Drawing.Point(86, 97);
+            this.cbSourceList.Name = "cbSourceList";
+            this.cbSourceList.Size = new System.Drawing.Size(155, 21);
+            this.cbSourceList.TabIndex = 0;
+            this.cbSourceList.SelectedIndexChanged += new System.EventHandler(this.cbSourceList_SelectedIndexChanged);
+            // 
+            // tbMapping
+            // 
+            this.tbMapping.BackColor = System.Drawing.Color.Silver;
+            this.tbMapping.Controls.Add(this.dataGridView1);
+            this.tbMapping.Controls.Add(this.label1);
+            this.tbMapping.Controls.Add(this.txtDestinationTableName);
+            this.tbMapping.Location = new System.Drawing.Point(4, 22);
+            this.tbMapping.Name = "tbMapping";
+            this.tbMapping.Padding = new System.Windows.Forms.Padding(3);
+            this.tbMapping.Size = new System.Drawing.Size(900, 643);
+            this.tbMapping.TabIndex = 1;
+            this.tbMapping.Text = "Mapping";
+            this.tbMapping.Enter += new System.EventHandler(this.tbMapping_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(165, 251);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next >>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtDestinationTableName
+            // 
+            this.txtDestinationTableName.Location = new System.Drawing.Point(179, 24);
+            this.txtDestinationTableName.Name = "txtDestinationTableName";
+            this.txtDestinationTableName.Size = new System.Drawing.Size(205, 20);
+            this.txtDestinationTableName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose Destination Table Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(844, 559);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 693);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tbSource.ResumeLayout(false);
+            this.tbSource.PerformLayout();
+            this.tbMapping.ResumeLayout(false);
+            this.tbMapping.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbSource;
+        private System.Windows.Forms.TabPage tbMapping;
+        private System.Windows.Forms.ComboBox cbSourceList;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDestinationTableName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DeltaExpress/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DeltaExpress
+{
+
+    public partial class Form1 : Form
+    {
+        public string filename;
+        // Create a new DataSet  
+        DataSet dtSet = new DataSet();
+        string CSVColsRows;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            filename = openFileDialog1.FileName;
+            txtFilePath.Text = filename;
+
+        }
+
+        private void tbSource_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbSourceList.SelectedIndex = 0;
+            btnSelectFile.Visible = false;
+            txtFilePath.Visible = false;
+            tabControl1.SelectTab("tbSource");
+
+        }
+
+        private void cbSourceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbSourceList.SelectedItem.ToString() == "CSV")
+            {
+                btnSelectFile.Visible = true;
+                txtFilePath.Visible = true;
+            }
+            else
+            {
+                btnSelectFile.Visible = false;
+                txtFilePath.Visible = false;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab("tbMapping");
+        }
+
+        private void tbMapping_Enter(object sender, EventArgs e)
+        {
+            FileInfo fi = new FileInfo(filename);
+            txtDestinationTableName.Text = fi.Name.Replace(fi.Extension, "");
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                CSVColsRows = sr.ReadLine();
+            }
+
+                // Create a new DataTable.    
+                DataTable custTable = new DataTable("Mappings");
+            DataColumn dtColumn;
+            DataRow myDataRow;
+
+            
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(bool);
+            dtColumn.ColumnName = "SelectColumn";
+            dtColumn.Caption = "Select Column";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "SourceColumn";
+            dtColumn.Caption = "Source Column";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = true;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Address column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "ColumnDelimeter";
+            dtColumn.Caption = "Column Delimeter";
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            custTable.Columns.Add(dtColumn);
+
+            // Create Address column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "ColumnType";
+            dtColumn.Caption = "Column Data Type";
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            custTable.Columns.Add(dtColumn);
+
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "DestinationColumn";
+            dtColumn.Caption = "Destination Column";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(bool);
+            dtColumn.ColumnName = "kEYColumn";
+            dtColumn.Caption = "Key Column";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            string[] colsList = CSVColsRows.Split(',');
+
+            foreach (string col in colsList)
+            { 
+                myDataRow = custTable.NewRow();
+                myDataRow["SelectColumn"] = true;
+                myDataRow["SourceColumn"] = col.Replace("\"", "");
+                myDataRow["ColumnDelimeter"] = ",";
+                myDataRow["ColumnType"] = "SQLNVARCHAR";
+                myDataRow["DestinationColumn"] = col.Replace("\"", "");
+                myDataRow["KeyColumn"] = false;
+
+                custTable.Rows.Add(myDataRow);
+            }
+
+            // Add custTable to the DataSet.    
+            dtSet.Tables.Add(custTable);
+
+
+            // Create a BindingSource  
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dtSet.Tables["Mappings"];
+
+            // Bind data to DataGridView.DataSource  
+            dataGridView1.DataSource = bs;
+        }
+    }
+}
+
